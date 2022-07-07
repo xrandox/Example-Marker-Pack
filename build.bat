8b21cf7c8fccf260db01cb921c626ef1c07459ee
null <xrandox17@yahoo.com>
2022-07-07 16:33:18 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,11 @@
-:: You must replace YOUR PACKS NAME and YOUR XMLS NAME in order to use
+:: You must replace <YOUR PACKS NAME> and <YOUR XMLS NAME> in order to use
+:: ex: del ".\TehsTrails.taco"
 
+:: deletes existing pack
 del ".\<YOUR PACKS NAME>.taco"
 
+:: uses WinRar to create a zip out of \Data and \YOUR XML
 "%ProgramFiles%\WinRAR\WinRAR.exe" a -afzip -ep1 -ibck -r -y ".\<YOUR PACKS NAME>.zip" ".\Data" ".\<YOUR XMLS NAME>.xml"
 
+:: renames the zip to taco
 rename "<YOUR PACKS NAME>.zip" "<YOUR PACKS NAME>.taco"

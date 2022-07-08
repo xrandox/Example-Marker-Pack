421db32af1d3a082fbbdd8587aecb80ad736dddd
null <xrandox17@yahoo.com>
2022-07-07 22:37:54 -0500

    changes

--- ExamplePack/build.bat
+++ ExamplePack/build.bat
@@ -1,12 +1,11 @@
-::note: .\ gives relative directory -- i.e., the folder the .bat is in
+:: You must replace <YOUR PACKS NAME> and <YOUR XMLS NAME> in order to use
+:: ex: del ".\TehsTrails.taco"
 
-::deletes the old .taco if it exists
-::   ".\your-pack.taco"
-del ".\ExamplePack.taco"
+:: deletes existing pack
+del ".\<YOUR PACKS NAME>.taco"
 
-::this is a bunch of fancy jargon to run WINRAR via command line
-::"location of your WINRAR Folder"                           ".\your-pack.zip"   ".\Data"  ".\your-xml.xml"
-"%ProgramFiles%\WinRAR\WinRAR.exe" a -afzip -ep1 -ibck -r -y ".\ExamplePack.zip" ".\Data" ".\ExamplePack.xml"
+:: uses WinRar to create a zip out of \Data and \YOUR XML
+"%ProgramFiles%\WinRAR\WinRAR.exe" a -afzip -ep1 -ibck -r -y ".\<YOUR PACKS NAME>.zip" ".\Data" ".\<YOUR XMLS NAME>.xml"
 
-::renames the zip to .taco -- this is mostly for distribution purposes. if left as .zip some people get confused and try to extract the pack
-rename "ExamplePack.zip" "ExamplePack.taco"
+:: renames the zip to taco
+rename "<YOUR PACKS NAME>.zip" "<YOUR PACKS NAME>.taco"
